1e774bab84340d1db10e5f05cad63f4d8860e594
steveseguin <punkrawker@gmail.com>
2024-05-29 18:22:47 -0400

    .

--- android/src/main/kotlin/de/julianassmann/flutter_background/IsolateHolderService.kt
+++ android/src/main/kotlin/de/julianassmann/flutter_background/IsolateHolderService.kt
@@ -7,7 +7,7 @@ import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
 import android.content.Intent
-import android.content.pm.ServiceInfo
+//import android.content.pm.ServiceInfo
 import android.net.wifi.WifiManager
 import android.os.Build
 import android.os.IBinder
@@ -130,7 +130,8 @@ class IsolateHolderService : Service() {
             startForeground(
                 1,
                 notification,
-                ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE);
+		1
+ //               ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE);
         } else {
             startForeground(
                 1,
